e831faa5e99e9e01407bc8903f49b0d7a26a0948
MUHAMMAD AQIL <111202214562@mhs.dinus.ac.id>
2025-11-12 05:37:01 +0000

    size of this frame is leng:widght 12:8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { Card, CardContent } from "@/components/ui/card";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Card className="w-full max-w-4xl">
-        <CardContent className="p-8">
+      <Card className="w-full max-w-4xl aspect-[12/8]">
+        <CardContent className="p-8 h-full">
           {/* You can add content here */}
         </CardContent>
       </Card>
