2bdd0521237bd7b78fd338671f633b267198cee1
MUHAMMAD AQIL <111202214562@mhs.dinus.ac.id>
2025-11-12 05:39:20 +0000

    i want size ratio 16:9

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent } from "@/components/ui/card";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <Card className="w-full max-w-4xl aspect-[11/6]">
+      <Card className="w-full max-w-4xl aspect-[16/9]">
         <CardContent className="p-8 h-full">
           {/* You can add content here */}
         </CardContent>
