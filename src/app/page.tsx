318a804ca1ea088e69cac1f9ef2e4c470e6392d9
MUHAMMAD AQIL <111202214562@mhs.dinus.ac.id>
2025-11-12 05:43:36 +0000

    TO small per day

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,9 +20,9 @@ export default function Home() {
               months: 'w-full h-full flex',
               month: 'w-full h-full flex flex-col',
               table: 'w-full h-full flex flex-col',
-              head_row: 'flex justify-around',
+              head_row: 'flex justify-around py-2',
               row: 'flex flex-1 justify-around items-center w-full',
-              day: 'h-full w-full max-w-none',
+              day: 'h-full w-full max-w-none p-0',
               cell: 'h-full w-full max-w-none',
             }}
           />
